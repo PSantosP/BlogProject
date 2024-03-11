2cf1aa7b04e535ca1c8836a476de3787803ac8a7
Santos <swpark@neowiz.com>
2024-03-11 23:00:26 +0900

    Santos Init

---
+++ Source/SudojuProject/SudojuProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SudojuProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SudojuProject, "SudojuProject" );
